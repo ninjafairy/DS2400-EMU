63c6eaaf64339faa6910c9981b4bda47cd6b6cb6
ninjafairy <jadonerenda@yahoo.com>
2024-09-03 11:56:42 -0700

    Update DS2401.ino
    code cleanup, working point

--- DS2401.ino
+++ DS2401.ino
@@ -1,102 +1,76 @@
-unsigned long time0 = micros();
-bool resetPulse = false;
-bool ox33 = false;
-byte family = 1;                                      // value of 1 indicates an ESN
-byte serialNumber[] = {133, 100, 196, 27, 0, 0,};     // S/N value
-byte crc = 142;                                       // crc, if you change the S/N must recompute 
-byte ESN[] = {1, 133, 100, 196, 27, 0, 0, 142};
-//byte ESN[] = {255,255,255,255,0,0,0,0};
-byte famCode = 0b10100111;
-unsigned long time1 = 0;
-unsigned long time2 = 0;
-unsigned long time3 = 0;
 
+byte ESN[] = {1, 133, 100, 196, 27, 0, 0, 142};
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(8, INPUT_PULLUP);
-  pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
-  Serial.begin(9600);
-  time1 = micros();
-  Serial.print("Boot time: ");
-  Serial.println(time1-time0);
-  Serial.print("famCode: ");
-  Serial.println(famCode, BIN);
+  DDRB = DDRB | B00000000;        // B-PORT to all inputs 0x00 mask doesnt change anything but still required to function?
+  PORTB = PORTB | B00000001;      // PB0 (pin 8) PULLUP resistor on.
+  DDRD = DDRD | B00000100;        // PD2 (pin 2) OUTPUT
+  PORTD = B00000000;              // D-PORT all pins LOW
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-// RESET PULSE DETECTION
-  while (resetPulse == false){      //scanning for the reset pulse
-    if (!PINB){                     //when target pin goes low
-      time0 = micros();             //reset timer
-      while (!PINB){                //pause while pin is LOW
+// RESET PULSE DETECTION ----------------------------------------------------------------------------------------
+  bool resetPulse = false;
+  while (resetPulse == false){        //scanning for the reset pulse
+    if (!PINB & B00000001){           //when target pin goes LOW begin timing LOW pulse
+      unsigned long time0 = micros();               //timestamp of detected LOW
+      while (!PINB & B00000001){      //pause while pin is LOW
+        delayMicroseconds(1); 
+      }
+      unsigned long time1 = micros();               //timestamp when pin goes HIGH
+      if (time0 - time1 > 200){       //if pulse was longer than 200uS
+        resetPulse = true;            //flag resetPulse true to break out of scanning loop
+      }
+    }
+  }
+
+// COMMAND BYTE RECIEVING AND DECODING -------------------------------------------------------------------------
+  byte romCommand;
+  for (int i = 0; i < 8; i++){        //index counter for building the command byte
+    unsigned long time0;
+    while (PINB & B00000001){                     //wait for the pin to get pulled LOW
+      time0 = micros();               //set timestamp once LOW
+    }                  
+    while (!PINB & B00000001){                    //pause till pin goes HIGH again
+      delayMicroseconds(1);
+    }
+     unsigned long time1 = micros() - time0;         //calculate pulse time (better than setting a second timestamp first?)           
+    if (time1 < 30){                  //if it stayed LOW for less than 30uS its a 1
+      bitWrite(romCommand, i, 1);     //set bit and index to 1
+      delayMicroseconds(100-time1);   //get close to the 120uS cycle time
+    }
+    else {
+      bitWrite(romCommand, i, 0);     //else its a 0 
+    }
+  }
+
+  if (romCommand == 0x33){
+    sendRom();
+    // Serial.println("\n0x33 Detected!!");
+  }
+  else if(romCommand == 0x0F){
+    sendRom();
+    // Serial.println("\n0x0F Detected!!");
+  } 
+}
+
+void sendRom(void) {
+  for (int bite = 0; bite < 8  ; bite++){
+    for (int bitt = 0; bitt < 8; bitt++){
+      while(PINB){
         delayMicroseconds(1);
       }
-      time1 = micros();             //second timestamp
-      if (time0 - time1 > 200){     //if pulse was longer than 200uS
-        resetPulse = true;          //flag resetPulse true to break out of scanning loop
+      while(!PINB){
+        delayMicroseconds(1);
       }
+      if (bitRead(ESN[bite], bitt)){
+        PORTD = B00000100;
+      }
+      delayMicroseconds(60);
+      PORTD = B00000000;
     }
   }
-resetPulse = false;                 //reset flag for next round.
-
-//COMMAND BYTE RECIEVING AND DECODING
-for (int i = 7; i >= 0; i--){       //index counter for building the command byte
-
-  while (PINB){                     //wait for the pin to get pulled LOW
-    time1 = micros();               //set timestamp once LOW
-  }                  
-  while (!PINB){                    //pause till pin goes HIGH again
-    delayMicroseconds(1);
-  }
-  time2 = micros() - time1;         //calculate pulse time (better than setting a second timestamp first?)           
-  if (time2 < 30){                  //if it stayed LOW for less than 30uS its a 1
-    bitWrite(famCode, i, 1);        //set bit and index to 1
-    delayMicroseconds(100-time2);   //get close to the 120uS cycle time
-  }
-  else {
-    bitWrite(famCode, i, 0);        //else its a 0 
-  }
-  
-}
-//Serial.print("\n"); printBin(famCode);
-if (famCode == 0b11001100){
- // Serial.println("\n0x33 Detected!!");
-  ox33 = true;
-}
-
-
-  if (0x33){
-    // for (int bite = 0; bite < 8; bite++){
-    //   //Serial.print(ESN[bite], HEX);
-    //   //printBin(ESN[bite]);
-    //   for (int bitt = 7; bitt >= 0; bitt--){
-    //     Serial.print(bitRead(ESN[bite], bitt));
-    //   }
-    // }
-    for (int bite = 0; bite < 8  ; bite++){
-      for (int bitt = 0; bitt < 8; bitt++){
-          while(PINB){
-            delayMicroseconds(1);
-          }
-          while(!PINB){
-            delayMicroseconds(1);
-          }
-          if (bitRead(ESN[bite], bitt)){
-          digitalWrite(2, HIGH);
-          //Serial.println("1");
-          }
-          delayMicroseconds(60);
-          digitalWrite(2, LOW);
-
-
-      }
-    }
-  }
-  Serial.println("\n0x33 Detected!!");
 }
 
 void printBin(byte aByte) {
