d637b14c9b194054130964812a8fb83bb7076d8b
ninjafairy <jadonerenda@yahoo.com>
2024-10-02 11:49:55 -0600

    Update DS2401.ino

--- DS2401.ino
+++ DS2401.ino
@@ -1,31 +1,37 @@
 // DS2400 Emulator by Squoril 9/28/2024
+#define PB0 1   //PB0 = Pin D8 for a nano  = B0000001
+#define PD2 3   //PD2 = Pin D2 for a nano  = B0000100
+#define PB0m 0  // no shift needed for PB0 mask
+#define PD2m 2  // shift the 1 (001) twice to get (100) for masking
+
 byte ESN[] = {142, 0, 0, 27, 196, 100, 133, 1};   //64bit ROM contents
+
 void setup() {
-  DDRB = DDRB | B00000000;        // B-PORT to all inputs 0x00 mask doesnt change anything but still required to function?
-  PORTB = PORTB | B00000001;      // PB0 (pin 8) PULLUP resistor on.
+  DDRB |= 0 << PB0;        // B-PORT to all inputs 0x00 mask doesnt change anything but still required to function?
 }
 
 void loop() {
 
 // RESET PULSE DETECTION ----------------------------------------------------------------------------------------  Uses PB0 as input
   while (true){                       //scanning for the reset pulse
-    if (!PINB & B00000001){           //when target pin goes LOW begin timing LOW pulse
+    if (!PINB & PB0){           //when target pin goes LOW begin timing LOW pulse
       unsigned long time0 = micros(); //timestamp of detected LOW
       waitForHigh();
       if (time0 - micros() > 200){       //if pulse was longer than 200uS
         break;}
     }
   }
-  delayMicroseconds(50);    // can this be removed?
+  delayMicroseconds(50);    //Delay between end of reset pulse and presence pulse
 
-// PRESENSE PULSE RESPONSE -------------------------------------------------------------------------------------  Uses PB2 as output
+// PRESENSE PULSE RESPONSE -------------------------------------------------------------------------------------  Uses PB0 as output
 
-    DDRD = DDRD | B00000100;        //PD2 set to OUTPUT and defaults to LOW beginning presence pulse
+    DDRB |= 1 << PB0m;          //PB0 set to OUTPUT and defaults to LOW beginning presence pulse
     delayMicroseconds(150);
-    DDRD = DDRD ^ B00000100;        //Reset PD2 to input high impedance so it doesnt hold high
+    DDRB &= ~(1 << PB0m);       //Reset PB0 to input high impedance so it doesnt hold high and can be used as input again
     delayMicroseconds(150);
 
 // COMMAND BYTE RECIEVING --------------------------------------------------------------------------------------  Uses PB0 as input
+
   byte romCommand;
   unsigned long decodeTime = micros();
   unsigned long timestamp[16];
@@ -39,6 +45,7 @@ void loop() {
   }
 
 // COMMAND BYTE DECODING ---------------------------------------------------------------------------------------
+
   for (int i = 1; i < 16; i=i+2){             //start and end timestamps and step forward by 2 since 16 timestamps for 8 pulses starts and stops
     if ((timestamp[i]-timestamp[i-1])>50){    //i is end timestamp, subtract i-1 timestamp to get pulse width 
       bitWrite(romCommand, ((i-1)/2), 0);}
@@ -47,25 +54,27 @@ void loop() {
   }
 
 // COMMAND SWITCH ----------------------------------------------------------------------------------------------
+
   if(romCommand == 0xF0){
     searchRom();
   } 
 }
+// END OF LOOP -------------------------------------------------------------------------------------------------
 
 
 void waitForLow(void){
-  while (PINB & B00000001){}                   //pause while PB0 is HIGH
+  while (PINB & PB0){}                   //pause while PB0 is HIGH
 }
 
 void waitForHigh(void){
-  while (!PINB & B00000001){}                  //pause while PB0 is LOW
+  while (!PINB & PB0){}                  //pause while PB0 is LOW
 }
 
 void pullLowForZero(void){
-  DDRD = DDRD | B00000100;        //PB2 set to OUTPUT and defaults to LOW beginning Bit = 0 pulse
+  DDRB |= 1 << PB0m;            //PB0 set to OUTPUT and defaults to LOW beginning presence pulse
   delayMicroseconds(35);
-  DDRD = DDRD ^ B00000100;        //Reset PB2 to input high impedance so it doesnt hold high but is allowed to be pulled up
-  delayMicroseconds(5);           //safety delay might be able to remove
+  DDRB &= ~(1 << PB0m);         //Reset PB0 to input high impedance so it doesnt hold high and can be used as input again
+  delayMicroseconds(5);
 }
 
 void searchRom(void){
