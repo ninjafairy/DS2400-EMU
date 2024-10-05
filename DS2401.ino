dafcab8e86768fdb434b3928504f53cfd41b522e
ninjafairy <jadonerenda@yahoo.com>
2024-10-05 11:51:24 -0600

    Close to final for Attiny9
    Tested Arduino Nano code
    Modified Timing for 1us ticks
    Added clock setup and prescale code
    Builds suspectly small

--- DS2401.ino
+++ DS2401.ino
@@ -1,17 +1,17 @@
+#define F_CPU 8000000UL											//Internal 8MHz Ocilator, /1 prescale = 8MHz clkCPU
 #include "avr/io.h"
 #include "util/delay.h"
-#define PB0pin 1   //PB0 = Pin D8 for a nano  = B0000001
-#define PB0m 0  // no shift needed for PB0 mask
-#define F_CPU 16000000UL
+#define PB0pin 1												//PB0 = Pin D8 for a nano  = B0000001
+#define PB0m 0													// no shift needed for PB0 mask
 
-unsigned char ESN[] = {142, 0, 0, 27, 196, 100, 133, 1};   //64bit ROM contents
+uint8_t ESN[] = {142, 0, 0, 27, 196, 100, 133, 1};				//64bit ROM contents
+	
+void waitForLow(void){
+	while (PINB & PB0){}										//pause while PB0 is HIGH
+}
 
 void waitForHigh(void){
 	while (!PINB & PB0){}										//pause while PB0 is LOW
-}
-
-void waitForLow(void){
-	while (PINB & PB0){}										//pause while PB0 is HIGH
 }
 
 void pullLowForZero(void){
@@ -22,80 +22,66 @@ void pullLowForZero(void){
 }
 
 void searchRom(void){
-
 	for (int i=7; i>=0; i--){									//Index for ESN[] bytes
-    for (uint8_t j = 0; j < 8; ++j) {  // Loop through the 8 bits of the selected ESN[] byte
-	    uint8_t mask = 1 << j;			
+		for (uint8_t mask = 1; mask>0; mask <<= 1){             //Index mask for bits out of selected ESN[] byte
 			waitForLow();                                       //Wait for read pulse from master                             DS2401 Tx BitN
 			if(ESN[i] & mask){                                  //If current bit is a one
-			_delay_us(10);}										//Do not pull low for Zero Wait for read pulse to pass
-			else{                                               //It current bit wasn't a one
+			_delay_us(10);}                                     //Do not pull low for Zero Wait for read pulse to pass
+			else{                                               //It current bit wasnt a one
 			pullLowForZero();}
 			waitForLow();                                       //Wait for read pulse from master                             DS2401 Tx !BitN
 			if(ESN[i] & mask){                                  //If current bit is a one
-			pullLowForZero();}									//Send a Zero since were in the complement stage
+			pullLowForZero();}                                  //Send a Zero since were in the complement stage
 			else{
-			_delay_us(10);}										//Do not pull low for Zero Wait for read pulse to pass
-			waitForLow();                                       //Absorbing the Master Tx BitN                                Master Tx BitN
-			waitForHigh();										//Incompatible with multiple 1-wire units now
+			_delay_us(10);}                                     //Do not pull low for Zero Wait for read pulse to pass
+			waitForLow();                                       //Absorbing the Master Tx BitN                                Master Tx BitN device select
+			waitForHigh();                                      //Incompatible with multiple 1-wire units now
 		}
 	}
 }
-
-
-
+	
 int main(void){
-	//DDRB |= 0;												// B-PORT to all inputs 0x00 mask doesnt change anything but still required to function?
-	TCCR0B |= 2;												// Enable Time0 with /8 prescaling
-
+	//CLKMSR = 0;											//Defaults to 0 for internal 8mhz occilator?
+	CLKPSR = 0;												//Set /1 prescaler for max CPU speed on internal oscillator, Source clock = 8Mhz clkCPU = 8Mhz
+	TCCR0B = 0x2;											//Set /8 prescaler timer0 now counts 1us per tick            Timer0 clock = 1Mhz
+		
     while (1) 
     {
-// RESET PULSE DETECTION ----------------------------------------------------------------------------------------  Uses PB0 as input
-		while (1){										//scanning for the reset pulse
-			if (!PINB & 00000001){						//when target pin goes LOW begin timing LOW pulse
-				uint8_t time0 = TCNT0;					//timestamp of detected LOW
+// RESET PULSE DETECTION -- Reset Pulse nominal 480us will overflow 8-bit counter, need to use 16-bit -----------  Uses PB0 as input
+		while (1){                                              //scanning for the reset pulse
+			if (!PINB & PB0){                                   //when target pin goes LOW begin timing LOW pulse
+				TCNT0 = 0;                                      //reset Timer0 to 0, 8Mhz Attiny with /8 prescale will be 1us per tick
+				uint16_t time0 = TCNT0;                         //timestamp of detected LOW, TCNT0 atomic reads the high/low bytes of the timer, 65ms overflow
 				waitForHigh();
-				if (time0 - TCNT0 > 200){				//if pulse was longer than 200uS
+				if (TCNT0 - time0 > 200){                       //if pulse was longer than 200uS 8mhz /8 prescale 1 tick = 1us nominal ~480us
 				break;}
 			}
 		}
-		_delay_us(50);									//Delay between end of reset pulse and presence pulse
+		_delay_us(50);                                          //Delay between end of reset pulse and presence pulse
 
 // PRESENSE PULSE RESPONSE -------------------------------------------------------------------------------------  Uses PB0 as output
-
-		DDRB |= 1 << PB0m;								//PB0 set to OUTPUT and defaults to LOW beginning presence pulse
+		DDRB |= 1 << PB0m;                                      //PB0 set to OUTPUT and defaults to LOW beginning presence pulse
 		_delay_us(150);
-		DDRB &= ~(1 << PB0m);							//Reset PB0 to input high impedance so it doesnt hold high and can be used as input again
+		DDRB &= ~(1 << PB0m);                                   //Reset PB0 to input high impedance so it doesnt hold high and can be used as input again
 		_delay_us(150);
 
-// COMMAND BYTE RECIEVING --------------------------------------------------------------------------------------  Uses PB0 as input
-
+// COMMAND BYTE READ -------------------------------------------------------------------------------------------  Uses PB0 as input
 		uint8_t romCommand = 0;
-		TCNT0 = 0;
-		uint16_t timestamp[16];
-		for (int i = 0; i < 8; i++){                    //index counter for building the command byte
-			int pointer = i*2;
+		for (int i = 0; i < 8; i++){                            //index counter for building the command byte
 			waitForLow();
-			timestamp[pointer] = TCNT0;
+			TCNT0 = 0;
 			waitForHigh();
-			timestamp[pointer+1] = TCNT0;
-		}
-
-// COMMAND BYTE DECODING ---------------------------------------------------------------------------------------
-
-		for (int i = 1; i < 16; i=i+2){					//start and end timestamps and step forward by 2 since 16 timestamps for 8 pulses starts and stops
-			if ((timestamp[i]-timestamp[i-1])>50){		//i is end timestamp, subtract i-1 timestamp to get pulse width
-			}
-			else {
-			romCommand |= 1 << ((i-1)/2);
+			if (TCNT0L < 40){                                   //1us per tick, Master holds low for 65us for a 0, so less than 40us is going to be a 1 (10us nominal)
+				romCommand |= 1 << i;                           //Below threshold, set i index bit of command byte
 			}
 		}
+
 // COMMAND SWITCH ----------------------------------------------------------------------------------------------
-
-		if(romCommand == 0xF0){
+		if(romCommand == 0xF0){                                 //0xF0 SearchRom,  0X33 ReadRom, 0x0F legacy ReadRom for the 2400
 			searchRom();
 		}
-
-// END OF LOOP -------------------------------------------------------------------------------------------------
 	}
+	// END OF LOOP -------------------------------------------------------------------------------------------------
 }
+
+
