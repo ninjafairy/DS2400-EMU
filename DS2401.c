7c245f4617b95cea54d8c6f0161332d743e01a8f
ninjafairy <jadonerenda@yahoo.com>
2024-10-22 00:38:00 -0600

    Update DS2401.c
    bugfix, better initilization

--- DS2401.c
+++ DS2401.c
@@ -5,7 +5,7 @@
 #define PB0m 0													// no shift needed for PB0 mask
 
 uint8_t ESN[] = {142, 0, 0, 27, 196, 100, 133, 1};				//64bit ROM contents
-	
+
 void waitForLow(void){
 	while (PINB & PB0){
 	}										//pause while PB0 is HIGH
@@ -29,33 +29,35 @@ void searchRom(void){
 			waitForLow();                                       //Wait for read pulse from master                             DS2401 Tx BitN
 			if(ESN[i] & mask){                                  //If current bit is a one
 				_delay_us(10);									//Do not pull low for Zero Wait for read pulse to pass
-			}                                     
+			}
 			else{                                               //It current bit wasnt a one
 				pullLowForZero();
 			}
 			waitForLow();                                       //Wait for read pulse from master                             DS2401 Tx !BitN
 			if(ESN[i] & mask){                                  //If current bit is a one
 				pullLowForZero();								//Send a Zero since were in the complement stage
-			}                                  
+			}
 			else{
 				_delay_us(10);									//Do not pull low for Zero Wait for read pulse to pass
-			}                                     
+			}
 			waitForLow();                                       //Absorbing the Master Tx BitN                                Master Tx BitN device select
 			waitForHigh();                                      //Incompatible with multiple 1-wire units now
 		}
 	}
 }
-	
+
 int main(void){
-	//CLKMSR = 0;												//Defaults to 0 for internal 8mhz occilator?
+	CLKMSR = 0;													//Defaults to 0 for internal 8mhz oscillator?
+	CCP = 0xD8;													//5.9.1. Configuration Change Protection Register
 	CLKPSR = 0;													//Set /1 prescaler for max CPU speed on internal oscillator, Source clock = 8Mhz clkCPU = 8Mhz
-	TCCR0B = 0x2;												//Set /8 prescaler timer0 now counts 1us per tick            Timer0 clock = 1Mhz
+	TCCR0B |= 1 << CS01;										//Set /8 prescaler timer0 now counts 1us per tick            Timer0 clock = 1Mhz
+	DDRB |= 1 << 2;
 		
-    while (1) 
-    {
-// RESET PULSE DETECTION -- Reset Pulse nominal 480us will overflow 8-bit counter, need to use 16-bit -----------  Uses PB0 as input
+	while (1)
+	{
+		// RESET PULSE DETECTION -- Reset Pulse nominal 480us will overflow 8-bit counter, need to use 16-bit -----------  Uses PB0 as input
 		while (1){                                              //scanning for the reset pulse
-			if (!PINB & PB0){                                   //when target pin goes LOW begin timing LOW pulse
+			if (!PINB & PB0pin){                                   //when target pin goes LOW begin timing LOW pulse
 				TCNT0 = 0;                                      //reset Timer0 to 0, 8Mhz Attiny with /8 prescale will be 1us per tick
 				uint16_t time0 = TCNT0;                         //timestamp of detected LOW, TCNT0 atomic reads the high/low bytes of the timer, 65ms overflow
 				waitForHigh();
@@ -66,13 +68,13 @@ int main(void){
 		}
 		_delay_us(50);                                          //Delay between end of reset pulse and presence pulse
 
-// PRESENSE PULSE RESPONSE -------------------------------------------------------------------------------------  Uses PB0 as output
+		// PRESENSE PULSE RESPONSE -------------------------------------------------------------------------------------  Uses PB0 as output
 		DDRB |= 1 << PB0m;                                      //PB0 set to OUTPUT and defaults to LOW beginning presence pulse
 		_delay_us(150);
 		DDRB &= ~(1 << PB0m);                                   //Reset PB0 to input high impedance so it doesnt hold high and can be used as input again
 		_delay_us(150);
 
-// COMMAND BYTE READ -------------------------------------------------------------------------------------------  Uses PB0 as input
+		// COMMAND BYTE READ -------------------------------------------------------------------------------------------  Uses PB0 as input
 		uint8_t romCommand = 0;
 		for (int i = 0; i < 8; i++){                            //index counter for building the command byte
 			waitForLow();
@@ -83,12 +85,10 @@ int main(void){
 			}
 		}
 
-// COMMAND SWITCH ----------------------------------------------------------------------------------------------
+		// COMMAND SWITCH ----------------------------------------------------------------------------------------------
 		if(romCommand == 0xF0){                                 //0xF0 SearchRom,  0X33 ReadRom, 0x0F legacy ReadRom for the 2400
 			searchRom();
 		}
 	}
-// END OF LOOP -------------------------------------------------------------------------------------------------
+	// END OF LOOP -------------------------------------------------------------------------------------------------
 }
-
-
